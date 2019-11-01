f95e2b81ae3665daafd3c10bdcc05ab1bf14a953
Jeffrey Layfield <jlayfield85@gmail.com>
2019-11-01 14:00:45 -0400

    added friends.js and the friends array

---
+++ app/data/friends.js
@@ -0,0 +1,92 @@
+var friends = [
+    {
+        "name": "Michael Scott",
+        "photo": "https://vignette.wikia.nocookie.net/the-office-us/images/3/34/Image.png/revision/latest?cb=20170829004901",
+        "scores" :[
+            5,
+            4,
+            4,
+            5,
+            5,
+            5,
+            3,
+            4,
+            5,
+            5
+          ]
+      
+},
+{
+    "name": "Jim Halpert",
+    "photo": "https://vignette.wikia.nocookie.net/the-office-us/images/0/0c/Jimhalpert1.jpg/revision/latest?cb=20170904172346",
+    "scores": [
+        3,
+        4,
+        4,
+        2,
+        5,
+        3,
+        2,
+        2,
+        4,
+        4
+      ]
+  
+}, 
+{
+    "name": "Pam Beesley",
+    "photo": "https://vignette.wikia.nocookie.net/theoffice/images/6/67/Pam_Beesley.jpg/revision/latest/scale-to-width-down/1000?cb=20170701084358",
+    "scores": [
+        2,
+        4,
+        4,
+        1,
+        2,
+        3,
+        4,
+        3,
+        4,
+        5
+      ]
+  
+}
+, 
+{
+    "name": "Dwight Scrute",
+    "photo": "https://vignette.wikia.nocookie.net/theoffice/images/c/c5/Dwight_.jpg/revision/latest/scale-to-width-down/1000?cb=20170701082424",
+    "scores": [
+        5,
+        3,
+        3,
+        5,
+        1,
+        5,
+        5,
+        5,
+        2,
+        1
+      ]
+  
+}
+
+, 
+{
+    "name": "Stanley Hudson",
+    "photo": "https://vignette.wikia.nocookie.net/theoffice/images/2/23/Stanley_Hudson.jpg/revision/latest/scale-to-width-down/1000?cb=20170701085445",
+    "scores": [
+        2,
+        3,
+        3,
+        4,
+        2,
+        1,
+        4,
+        3,
+        2,
+        3
+      ]
+  
+}
+];
+
+module.exports = friends;
