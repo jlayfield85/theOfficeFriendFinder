1f1bdc5ccd79e2e71881e2b945153318210e48f1
Jeffrey Layfield <jlayfield85@gmail.com>
2019-11-01 14:35:13 -0400

    editing survey.html page

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express ();
 var bodyParser = require('body-parser')
-var PORT = process.env.PORT || 6000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static("app/public"));
 
